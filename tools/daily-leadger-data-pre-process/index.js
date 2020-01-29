bfbe868a3e3fd635f4aa4df863218b276671d833
KiyonamiYu <514723273@qq.com>
2020-01-29 17:29:30 +0800

    init again

--- tools/daily-leadger-data-pre-process/index.js
+++
@@ -1,6 +0,0 @@
-const readToday = require('./main/file-process');
-
-const TODAY_FILE_PATH = "../../today.txt";
-const DATA_FILE_PATH = "../../public/data";
-
-readToday(TODAY_FILE_PATH, DATA_FILE_PATH);
